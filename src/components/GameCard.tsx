ea1edcfe9e4a9865feb3b4a2e76aa03f155b110f
Ståle <staale_80@hotmail.com>
2024-02-02 11:18:12 +0100

    Refactor: Style simplification

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -8,7 +8,7 @@ interface Props {
     game: Game,
 }
 const GameCard = ({ game }: Props) => {
-    return <Card borderRadius={10} overflow='hidden' width='300px'>
+    return <Card>
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react"
 import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
+import GameCardContainer from "./GameCardContainer"
 
 
 
@@ -15,10 +16,16 @@ const GameGrid = () => {
             <SimpleGrid columns={{
                 sm: 1, md: 2, lg: 3, xl: 5
             }} spacing={10} padding='10px'>
-                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
-                {gameList.map(game => (
-                    <GameCard key={game.id} game={game} />
-                ))}
+                {isLoading && skeletons.map(skeleton => <GameCardContainer>
+                    <GameCardSkeleton key={skeleton} />
+                </GameCardContainer>)}
+                {
+                    gameList.map(game => (
+                        <GameCardContainer>
+                            <GameCard key={game.id} game={game} />
+                        </GameCardContainer>
+                    ))
+                }
             </SimpleGrid>
         </>)
 }
