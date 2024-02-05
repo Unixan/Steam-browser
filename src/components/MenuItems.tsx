79ff4a21bfe84f79dffe6124fd2572bc98430076
Ståle <staale_80@hotmail.com>
2024-02-05 13:09:18 +0100

    Fetch genres from API

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,20 +1,8 @@
-import { SimpleGrid } from "@chakra-ui/react"
-import { NavLink } from "react-router-dom"
-import "../styles.css"
+import GenreList from "./GenreList"
 
 const MenuItems = () => {
     return (
-
-        <nav id='menu'>
-            <SimpleGrid columns={1} spacing={{
-                base:"20px",
-                lg: "50px"
-            }} textAlign='center'>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/Players'>Players</NavLink>
-                <NavLink to='/Games'>Games</NavLink>
-            </SimpleGrid>
-        </nav >
+    <GenreList/>        
     )
 }
 
